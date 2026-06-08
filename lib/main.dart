5913d1f96b2815ec850ebd92ff63eb1f4cf587b4
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2026-06-08 02:18:59 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('My VPN App')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              print('Connect Button Pressed!');
+            },
+            child: const Text('Connect'),
+          ),
+        ),
+      ),
+    );
+  }
+}
